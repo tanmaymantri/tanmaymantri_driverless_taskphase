ae88be8c81c552ad9fa794737ef2faf1f314d754
tanmaymantri <tanmay5.mitmpl2026@learner.manipal.edu>
2026-08-31 19:02:06 +0530

    Implement Selection Sort and Binary Search functionality
    Added SelectionSort and BinarySearch classes for sorting and searching words.

---
+++ Q3.py
@@ -0,0 +1,41 @@
+class SelectionSort:
+    def sort(self, words):
+        n = len(words)
+
+        for i in range(n):
+            index = i
+            for j in range(i+1, n):
+                if words[j] < words[index]:
+                    index = j
+            new = words[i]
+            words[i] = words[index]
+            words[index] = new
+        return words
+
+class BinarySearch:
+    def search(self, words, target):
+        low = 0
+        high = len(words) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if words[mid] == target:
+                return True
+            elif words[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
+
+words = input("Enter words: ").split()
+obj = SelectionSort()
+words = obj.sort(words)
+print("Sorted words:", words)
+
+target = input("Enter word to search: ")
+search_object = BinarySearch()
+result = search_object.search(words, target)
+print(result)
+if result:
+    print("String found")
+else:
+    print("String not found")
